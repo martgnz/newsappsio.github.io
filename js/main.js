a1b3bea788eb4f2e1fe600994dcbe174155c2ba1
Martín González <m@martingonzalez.net>
2015-12-26 11:13:00 +0100

    lazyload, minify animation css, move header back to the center, maximize container size

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ wow = new WOW({ boxClass: 'scroll', animateClass: 'animated', offset: 0, mobile:
 
 // initiate wow
 wow.init()
+
+// lazyload
+var layzr = new Layzr({
+  selector: '[data-img]',
+  attr: 'data-img',
+  retinaAttr: 'data-img-retina',
+  bgAttr: 'data-img-bg',
+  hiddenAttr: 'data-img-hidden',
+  threshold: 0,
+  callback: null
+});
