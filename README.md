bd6e4084b1e8f5294a489fe06c083264640200e9
Martín González <m@martingonzalez.net>
2016-01-18 22:56:02 +0100

    implement fb and twtr meta tags

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# newsappsio.github.io
+# newsappsio website
+Our (still WIP) website. Uses [Jekyll](https://jekyllrb.com/) and [Bootstrap 4](http://v4-alpha.getbootstrap.com/).
--- js/main.js
+++ js/main.js
@@ -5,22 +5,7 @@ $("img").unveil(200, function() {
   });
 });
 
-// anchor scroll
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 300);
-        return false;
-      }
-    }
-  });
-});
-
+// for the lulz
 console.log("░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░")
 console.log("░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░")
 console.log("░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░")
